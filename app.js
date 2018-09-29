fb288657773e67cdaf9e7fa2bd54aa0e16760356
primoz <primoz969@gmail.com>
2018-09-29 12:29:47 +0200

    hides the dice before rolling the dice for first time

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ roundScore = 0;
 activePlayer = 0;
 
 dice = Math.floor(Math.random() * 6) + 1;
+
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#current-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
