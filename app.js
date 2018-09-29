53f10a262dabc5f0d9801fb210b2bf53476fdeb0
primoz <primoz969@gmail.com>
2018-09-29 14:21:34 +0200

    changes player on rolling one and styles

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
-
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. random number
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -26,6 +29,34 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
     
     // 3. update the round score if the rolled number was not a 1
+    if(dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //Add Current score to Global score
+
+    //Update the UI
+
+    //Chewck if player won the game
 });
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
